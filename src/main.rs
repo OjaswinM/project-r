7f8f891a3295ab19848cdb082b48751adce4089d
OjaswinM <ojaswin@hotmail.com>
2020-04-27 17:05:17 +0530

    Change entry point of the program to _start()
    This will be the default entry point of our kernel. This function is
    marked with 'extern "C"' to ensure that C calling conventions are used.

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,18 @@
 #![no_std]
+#![no_main]
 
 use core::panic::PanicInfo;
 
 // TODO: Implement a proper panic handler
-fn panic(info: &PanicInfo) {
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
     loop {}
 }
 
-
-fn main() {
+// TODO: Implement the kernel _start
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    loop {}
 }
 
 
