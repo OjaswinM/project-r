d1cefa7629c46b9151963c106389ce4e0b738db5
OjaswinM <ojaswin@hotmail.com>
2020-04-29 13:04:08 +0530

    Add dummy start code

--- src/main.rs
+++ src/main.rs
@@ -9,9 +9,20 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
-// TODO: Implement the kernel _start
+static HELLO: &[u8] = b"Hello World!";
+
+// TODO: Implement the kernel main()
 #[no_mangle]
-pub extern "C" fn _start() -> ! {
+pub extern "C" fn main() -> ! {
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
     loop {}
 }
 
