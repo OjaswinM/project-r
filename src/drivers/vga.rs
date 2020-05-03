99a5e4a0d920ca925b75eb76aee562ce4bef0626
Ojaswin Mujoo <ojaswin25111998@gmail.com>
2020-05-04 00:37:04 +0530

    Follow Rust style guidelines

--- src/drivers/vga.rs
+++ src/drivers/vga.rs
@@ -2,12 +2,12 @@
 
 const VGA_MEM_START: u32 = 0xb8000;
 const VGA_COLS: usize = 80;
-const VGA_ROWS: usize = 24;
+const VGA_ROWS: usize = 25;
 
 #[allow(dead_code)]
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 #[repr(u8)]
-pub enum color_codes {
+pub enum ColorCodes {
     Black = 0,
     Blue = 1,
     Green = 2,
@@ -15,35 +15,35 @@ pub enum color_codes {
     Red = 4,
     Magenta = 5,
     Brown = 6,
-    Lt_gray = 7,
-    Dark_gray = 8,
-    Lt_blue = 9,
-    Lt_green = 10,
-    Lt_cyan = 11,
-    Lt_red = 12,
+    LightGray = 7,
+    DarkGray = 8,
+    LightBlue = 9,
+    LightGreen = 10,
+    LightCyan = 11,
+    LightRed = 12,
     Pink = 13,
     Yellow = 14,
     White = 15,
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
-struct vga_color(u8);
+struct VgaColor(u8);
 
-impl vga_color {
-	fn new(fg: color_codes, bg: color_codes) -> vga_color {
-		vga_color((bg as u8) << 4 | (fg as u8))
+impl VgaColor {
+	fn new(fg: ColorCodes, bg: ColorCodes) -> VgaColor {
+		VgaColor((bg as u8) << 4 | (fg as u8))
 	}
 }
 
 #[repr(C)]
-struct vga_entry {
+struct VgaEntry {
 	character: u8,
-	color: vga_color,
+	color: VgaColor,
 }
 
-impl vga_entry {
-	fn new(char: u8, color: vga_color) -> vga_entry {
-		vga_entry { 
+impl VgaEntry {
+	fn new(char: u8, color: VgaColor) -> VgaEntry {
+		VgaEntry { 
 			character: char,
 			color: color,
 		}		
@@ -51,36 +51,36 @@ impl vga_entry {
 }
 
 #[repr(transparent)]
-struct vga_buffer {
-	buf: [[vga_entry; VGA_COLS]; VGA_ROWS],
+struct VgaBuffer {
+	buf: [[VgaEntry; VGA_COLS]; VGA_ROWS],
 }
 
-struct vga_struct {
-	buffer: &'static mut vga_buffer,
-	color: vga_color,
+struct VgaStruct {
+	buffer: &'static mut VgaBuffer,
+	color: VgaColor,
 }
 
-impl vga_struct {
+impl VgaStruct {
 	
 	pub fn cls(&mut self) -> () {
 		for i in 0..VGA_ROWS  {
 			for j in 0..VGA_COLS {
 				// 32 is the ASCII value for <Space>
-				self.buffer.buf[i][j] = vga_entry::new(32, self.color);
+				self.buffer.buf[i][j] = VgaEntry::new(32, self.color);
 			}
 		}
 	}
 
 	pub fn put(&mut self, char: u8, row: usize, col: usize) -> () {
-		self.buffer.buf[row][col] = vga_entry::new(char, self.color)
+		self.buffer.buf[row][col] = VgaEntry::new(char, self.color)
 	}
 }
 
 
 pub fn vga_put(char: u8, row: usize, col: usize) -> () {
-	let mut vga = vga_struct {
-		buffer: unsafe { &mut *(VGA_MEM_START as *mut vga_buffer) },
-		color: vga_color::new(color_codes::White, color_codes::Black),
+	let mut vga = VgaStruct {
+		buffer: unsafe { &mut *(VGA_MEM_START as *mut VgaBuffer) },
+		color: VgaColor::new(ColorCodes::Yellow, ColorCodes::Black),
 	};
 
 	vga.put(char, row, col);
